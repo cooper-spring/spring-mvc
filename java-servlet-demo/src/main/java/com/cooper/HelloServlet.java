f380cb40f7d63681407a02122e5d0e7409f9cc75
cooper <pbg0205@naver.com>
2022-05-26 01:47:26 +0900

    feat: 서블릿 어플리케이션을 테스트를 위한 설정 추가

---
+++ java-servlet-demo/src/main/java/com/cooper/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.cooper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("HelloServlet.init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HelloServlet.doGet");
+
+        resp.getWriter().println("<html>");
+        resp.getWriter().println("<head>");
+        resp.getWriter().println("<body>");
+        resp.getWriter().println("<h1> Hello Servlet </h1>");
+        resp.getWriter().println("</body>");
+        resp.getWriter().println("</head>");
+        resp.getWriter().println("</html>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("HelloServlet.destroy");
+    }
+
+}
